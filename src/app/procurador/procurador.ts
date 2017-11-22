42696b3402262f33f3992b1d50457c520f19a32c
prmorais13 <prmorais_13@hotmail.com>
2017-11-22 17:03:46 -0300

    Quinto commit - Trabalho

--- src/app/procurador/procurador.ts
+++ src/app/procurador/procurador.ts
@@ -7,11 +7,12 @@ export class Procurador {
     dataCriacao: Date;
 
     constructor(id: number, nome: string, procuradoria: string,
-        email: string, celular: string) {
+        email: string, celular: string, dataCriacao: Date) {
         this.id = id;
         this.nome = nome;
         this.procuradoria = procuradoria;
         this.email = email;
         this.celular = celular;
+        this.dataCriacao = dataCriacao;
     }
 }
